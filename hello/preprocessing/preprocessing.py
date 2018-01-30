6c26e309004e2e0c5e41ed6220a8aebf06c060ec
Juan Alvarado <juaelalv@espol.edu.ec>
2018-01-30 08:43:08 -0500

    Cropping image

--- hello/preprocessing/preprocessing.py
+++ hello/preprocessing/preprocessing.py
@@ -1,9 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageChops
 from resizeimage import resizeimage
 import numpy as np
 
+def trim(im):
+    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
 img = Image.open("../media/img011-00011.png")
-img_resize = resizeimage.resize_contain(img, [28, 28])
-gray = img_resize.convert('L')
+gray = img.convert('L')
 bw = gray.point(lambda x: 255 if x<128 else 0, '1')
-bw.save("../media/preprocessing_data/result_bw.png")
+cropp = trim(bw)
+img_resize = resizeimage.resize_cover(cropp, [28, 28])
+img_resize.save("../media/preprocessing_data/result_bw.png")
+
